4642157af684d4d7641ca7518f33a76346ca3fe7
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-05-13 18:46:07 +0530

    added common groovy scripts

--- src/com/oracle/fmwk8s/common/Common.groovy
+++ src/com/oracle/fmwk8s/common/Common.groovy
@@ -6,7 +6,7 @@ class Common {
     static def getUniqueId(def script) {
         def date = new Date()
         def sdf = new SimpleDateFormat("MMddHHmmss")
-        
+
         def buildNumber = "${script.env.BUILD_NUMBER}"
         def uniqueId = sdf.format(date) + "." + buildNumber
 
