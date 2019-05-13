6b43e9a1d01586fc697bc36948bb2b802eebd8bc
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-05-13 17:53:31 +0530

    log var handling

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,3 +1,10 @@
+def call(String level = 'INFO', String message = '') {
+    if (level.equals('INFO'))
+        info(message)
+    else if (level.equals('WARNING'))
+        warning(message)
+}
+
 def info(message) {
     echo "INFO: ${message}"
 }
