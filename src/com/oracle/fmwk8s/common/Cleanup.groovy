373d297a514732be563dfaad69428b43b05762de
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-05-17 11:56:40 +0530

    cleanup library

--- src/com/oracle/fmwk8s/common/Cleanup.groovy
+++ src/com/oracle/fmwk8s/common/Cleanup.groovy
@@ -12,7 +12,60 @@ class Cleanup {
         }
     }
 
-    static def cleanDomain(String namespace) {}
+    static def cleanDomain(String domainName, String namespace) {
+        try {
+            sh label: 'clean domain pods and services', script: '''
+                        kubectl delete jobs --all -n ${namespace}
+                        kubectl delete services --all -n ${namespace}
+                        kubectl delete pods --all -n ${namespace}
+                        '''
+        }
+        catch (exc) {
+            echo "Cleanup domain pods and services failed!!"
+        }
+        finally {
+            sleep 10
+        }
+
+        try {
+            sh label: 'clean domain configmap and stateful sets', script: '''
+                        kubectl delete configmaps --all -n ${namespace}
+                        kubectl delete statefulsets --all -n ${namespace}
+                        '''
+        }
+        catch (exc) {
+            echo "Cleanup domain configmap and stateful sets failed!!"
+        }
+        finally {
+            sleep 30
+        }
+
+        try {
+            sh label: 'clean domain resource', script: '''
+                        kubectl delete domain ${domainName} -n ${namespace}
+                        '''
+        }
+        catch (exc) {
+            echo "Cleanup domain resource failed!!"
+        }
+        finally {
+            sleep 30
+        }
+
+        try {
+            sh label: 'clean domain persistent volume', script: '''
+                        kubectl delete pvc ${domainName}-${namespace}-pvc -n ${namespace}
+                        sleep 10
+                        kubectl delete pv ${domainName}-${namespace}-pv -n ${namespace}
+                        '''
+        }
+        catch (exc) {
+            echo "Cleanup domain persistent volume failed!!"
+        }
+        finally {
+            sleep 10
+        }
+    }
 
     static def cleanDatabase(String namespace) {}
 }
