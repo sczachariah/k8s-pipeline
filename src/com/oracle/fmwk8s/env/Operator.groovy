77c846e0a50ba05c0bb1277911773aa077a82460
srsanaga <srivalli.sanagavarapu@oracle.com>
2019-06-11 16:46:29 +0530

    Operator functions

--- src/com/oracle/fmwk8s/env/Operator.groovy
+++ src/com/oracle/fmwk8s/env/Operator.groovy
@@ -6,22 +6,26 @@ class Operator {
     static buildOperator(script,docker_username,docker_password,https_proxy) {
         try {
             Log.info(script, "Build soa operator image!!!")
-            script.sh "docker login http://container-registry.oracle.com -u ${docker_username} -p ${docker_password}"
+            withCredentials([[$class          : 'UsernamePasswordMultiBinding', credentialsId: 'DockerHub',
+                              usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD'],
+                             [$class          : 'UsernamePasswordMultiBinding', credentialsId: 'sandeep.zachariah.docker',
+                              usernameVariable: 'DOCKER_USERNAME_CISYSTEM', passwordVariable: 'DOCKER_PASSWORD_CISYSTEM']]) {
+                script.sh "docker login http://container-registry.oracle.com -u ${DOCKER_USERNAME_CISYSTEM} -p ${DOCKER_PASSWORD_CISYSTEM}"
 
-            script.sh "docker pull container-registry.oracle.com/java/serverjre:latest"
-            script.sh "docker tag container-registry.oracle.com/java/serverjre:latest store/oracle/serverjre:8"
+                script.sh "docker pull container-registry.oracle.com/java/serverjre:latest"
+                script.sh "docker tag container-registry.oracle.com/java/serverjre:latest store/oracle/serverjre:8"
 
-            script.sh "docker pull fmw-cert-docker.dockerhub-den.oraclecorp.com/soaoperatorpoc/weblogic-kubernetes-operator:2.1"
-            script.sh "docker tag fmw-cert-docker.dockerhub-den.oraclecorp.com/soaoperatorpoc/weblogic-kubernetes-operator:2.1 weblogic-kubernetes-operator:2.1"
+                script.sh "docker pull fmw-cert-docker.dockerhub-den.oraclecorp.com/soaoperatorpoc/weblogic-kubernetes-operator:2.1"
+                script.sh "docker tag fmw-cert-docker.dockerhub-den.oraclecorp.com/soaoperatorpoc/weblogic-kubernetes-operator:2.1 weblogic-kubernetes-operator:2.1"
 
 
-            script.sh "docker build --build-arg https_proxy=${https_proxy} -t soa-kubernetes-operator:2.1 --no-cache=true ."
+                script.sh "docker build --build-arg https_proxy=${https_proxy} -t soa-kubernetes-operator:2.1 --no-cache=true ."
 
-            Log.info(script, "Push soa operator image!!!")
-            script.sh "docker tag soa-kubernetes-operator:2.1 cisystem.docker.oraclecorp.com/soa-kubernetes-operator:2.1"
-            script.sh "docker login cisystem.docker.oraclecorp.com -u ${docker_username} -p ${docker_password}"
-            script.sh "docker push cisystem.docker.oraclecorp.com/soa-kubernetes-operator:2.1"
-
+                Log.info(script, "Push soa operator image!!!")
+                script.sh "docker tag soa-kubernetes-operator:2.1 cisystem.docker.oraclecorp.com/soa-kubernetes-operator:2.1"
+                script.sh "docker login cisystem.docker.oraclecorp.com -u ${docker_username} -p ${docker_password}"
+                script.sh "docker push cisystem.docker.oraclecorp.com/soa-kubernetes-operator:2.1"
+            }
         }
         catch (exc) {
             Log.error(script, "Build operator failed!!.")
