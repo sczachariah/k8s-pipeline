f36dfcf94efdcc81197ca4e7ae5c5614ce11c0c2
srsanaga <srivalli.sanagavarapu@oracle.com>
2019-06-13 14:02:36 +0530

    Operator functions

--- src/com/oracle/fmwk8s/env/Operator.groovy
+++ src/com/oracle/fmwk8s/env/Operator.groovy
@@ -36,17 +36,9 @@ class Operator {
             script.sh "retVal=`echo \\`helm ls ${operator_rel}\\``"
 
             script.sh "if [[ !  -z  "$retVal" ]]; then"
-            script.sh       "helm upgrade \
-				              --reuse-values \
-				              --set domainNamespaces={$domainns} \
-				              --wait \
-				              ${operator_rel} \
-				              kubernetes/charts/soa-kubernetes-operator"
-            script.sh "else"
-            script.sh        "helm install kubernetes/charts/soa-kubernetes-operator \
-					             --name ${operator_rel} \
-					             --set image=cisystem.docker.oraclecorp.com/soa-kubernetes-operator:2.1 \
-					             --namespace ${operatorns} \
+            script.sh  "helm upgrade --reuse-values --set domainNamespaces={$domainns} --wait ${operator_rel} kubernetes/charts/soa-kubernetes-operator"
+            script.sh  "else"
+            script.sh  "helm install kubernetes/charts/soa-kubernetes-operator --name ${operator_rel} --set image=cisystem.docker.oraclecorp.com/soa-kubernetes-operator:2.1 --namespace ${operatorns} \
 					             --set serviceAccount=${operatorsa} \
 			    	             --set domainNamespaces={} \
 					             --wait"
