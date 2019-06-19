b751984ca11f7c64fcf4932f4c3b47591c6d1d80
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-06-19 10:01:12 +0530

    code for create NFS folder

--- src/com/oracle/fmwk8s/common/EnvironmentSetup.groovy
+++ src/com/oracle/fmwk8s/common/EnvironmentSetup.groovy
@@ -9,12 +9,12 @@ class EnvironmentSetup {
                     credentialsId: 'sandeep.zachariah.ssh',
                     url: 'git@orahub.oraclecorp.com:fmw-platform-qa/fmw-k8s-pipeline.git'
 
-            script.sh "export KUBECONFIG=${script.env.KUBECONFIG} \
-                        cd kubernetes/framework \
-                        sed -i \"s#%FMWK8S_NFS_HOME%#${nfsHomeDir}#g\" fmwk8s-mkdir-pod.yaml \
-                        sed -i \"s#%NFS_DOMAIN_DIR%#${nfsDomainDir}#g\" fmwk8s-mkdir-pod.yaml \
-                        cat fmwk8s-mkdir-pod.yaml \
-                        kubectl apply -f fmwk8s-mkdir-pod.yaml -n ${namespace}"  \
+            script.sh "export KUBECONFIG=${script.env.KUBECONFIG} && \
+                        cd kubernetes/framework && \
+                        sed -i \"s#%FMWK8S_NFS_HOME%#${nfsHomeDir}#g\" fmwk8s-mkdir-pod.yaml && \
+                        sed -i \"s#%NFS_DOMAIN_DIR%#${nfsDomainDir}#g\" fmwk8s-mkdir-pod.yaml && \
+                        cat fmwk8s-mkdir-pod.yaml && \
+                        kubectl apply -f fmwk8s-mkdir-pod.yaml -n ${namespace}"
 
         }
         catch (exc) {
