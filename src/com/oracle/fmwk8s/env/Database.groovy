dbf1c05ff7e96ba55d2542b3916cfd504d24b6b2
srsanaga <srivalli.sanagavarapu@oracle.com>
2019-06-24 13:48:15 +0530

    Library functions for DB deployment added

--- src/com/oracle/fmwk8s/env/Database.groovy
+++ src/com/oracle/fmwk8s/env/Database.groovy
@@ -6,25 +6,25 @@ class Database {
     static deployDatabase(script,domainns,REGISTRY_AUTH_USR,REGISTRY_AUTH_PSW) {
         try {
             Log.info(script, "setup env !!!")
+            script.git branch: 'master',
+                    credentialsId: 'sandeep.zachariah.ssh',
+                    url: 'git@orahub.oraclecorp.com:tooling/soa-kubernetes-operator.git'
             script.sh "export KUBECONFIG=${script.env.KUBECONFIG}"
             Log.info(script, "setup db !!!")
-            script.sh "retVal=`echo \\`kubectl get secret regcred -n ${domainns} 2>&1\\``"
-            script.sh "if echo \"\$retVal\" | grep -q \"not found\"; then\n \
+            script.sh "retVal=`echo \\`kubectl get secret regcred -n ${domainns} 2>&1\\`` &&\
+                      if echo '\$retVal' | grep -q 'not found'; then\n \
                           kubectl create secret docker-registry regcred -n ${domainns} --docker-server=http://container-registry.oracle.com --docker-username='${REGISTRY_AUTH_USR}' --docker-password='${REGISTRY_AUTH_PSW}' --docker-email='${REGISTRY_AUTH_USR}'\n \
-                       fi"
-            script.sh "cd kubernetes/samples/scripts/create-soa-domain/domain-home-on-pv/multiple-Managed-servers"
-            script.sh "ls"
-            script.sh "cp soadb.yaml soadb.yaml.orig"
-
-            script.sh "sed -i ""s#image: oracle/database:12.2.0.1#image: container-registry.oracle.com/database/enterprise:12.2.0.1-slim#g"" soadb.yaml"
-            script.sh "sed -i \"s#namespace: soans#namespace: ${domainns}#g\" soadb.yaml"
-            script.sh "sed -i \"s#terminationGracePeriodSeconds: 30#terminationGracePeriodSeconds: 30\\n      imagePullSecrets:\\n      - name: regcred#g\" soadb.yaml"
-            script.sh "cat soadb.yaml"
-
-            script.sh "kubectl apply -f soadb.yaml"
-
-            script.sh "dbstat='dbstat'"
-            script.sh "i=0"
+                      fi && \
+                      cd kubernetes/samples/scripts/create-soa-domain/domain-home-on-pv/multiple-Managed-servers && \
+                      ls && \
+                      cp soadb.yaml soadb.yaml.orig && \
+                      sed -i ""s#image: oracle/database:12.2.0.1#image: container-registry.oracle.com/database/enterprise:12.2.0.1-slim#g"" soadb.yaml &&\
+                      sed -i \"s#namespace: soans#namespace: ${domainns}#g\" soadb.yaml &&\
+                      sed -i \"s#terminationGracePeriodSeconds: 30#terminationGracePeriodSeconds: 30\\n      imagePullSecrets:\\n      - name: regcred#g\" soadb.yaml && \
+                      cat soadb.yaml && \
+                      kubectl apply -f soadb.yaml && \
+                      dbstat='dbstat' && \
+                      i=0"
             script.sh "until `echo \$dbstat | grep -q Running` > /dev/null\n \
                        do \n \
                        if [ \$i == 25 ]; then\n \
@@ -37,9 +37,8 @@ class Database {
                        dbstat=`echo \\`kubectl get pods -n ${domainns} 2>&1 | grep soadb\\``\n \
                        done"
             Log.info(script, "DB container is Running.")
-            script.sh "sleep 300"
-
-            script.sh "kubectl get pods,svc -n ${domainns} | grep soadb"
+            script.sh "sleep 300 && \
+                       kubectl get pods,svc -n ${domainns} | grep soadb"
 
             Log.info(script, "Deploy DB Completed!!!")
 
