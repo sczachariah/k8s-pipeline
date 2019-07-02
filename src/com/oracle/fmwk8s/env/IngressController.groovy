4199023f49155f1fc6a1e13f778a334d76ef202a
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-07-02 16:05:27 +0530

    handle diff ingress controllers

--- src/com/oracle/fmwk8s/env/IngressController.groovy
+++ src/com/oracle/fmwk8s/env/IngressController.groovy
@@ -1,4 +1,32 @@
 package com.oracle.fmwk8s.env
 
 class IngressController {
+
+    static deployLoadBalancer(script, lbType, domainNamespace) {
+        switch ("${lbType}") {
+            case "TRAEFIK":
+                deployTraefik(domainNamespace)
+                break
+            case "APACHE":
+                deployApache(domainNamespace)
+                break
+            case "VOYAGER":
+                deployVoyager(domainNamespace)
+                break
+            case "NGINX":
+                deployNginx(domainNamespace)
+                break
+            default:
+                deployTraefik(domainNamespace)
+                break
+        }
+    }
+
+    static deployTraefik(script, domainNamespace) {}
+
+    static deployApache(script, domainNamespace) {}
+
+    static deployVoyager(script, domainNamespace) {}
+
+    static deployNginx(script, domainNamespace) {}
 }
