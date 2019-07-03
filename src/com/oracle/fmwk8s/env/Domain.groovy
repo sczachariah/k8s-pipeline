cb276ff45e5dc538108c92f629e7b4cd74a99130
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-07-03 21:47:41 +0530

    correct readiness url

--- src/com/oracle/fmwk8s/env/Domain.groovy
+++ src/com/oracle/fmwk8s/env/Domain.groovy
@@ -195,7 +195,7 @@ class Domain {
 
             script.sh "kubectl get all,domains -n ${domainNamespace}"
             script.sh "kubectl get domain -n ${domainNamespace} | grep ${domainName}"
-            script.sh "curl -v \"http://${domainName}-adminserver.${domainNamespace}.svc.cluster.local:7001/weblogic/ready\" | grep 200"
+            script.sh "curl -v \"http://${domainName}-admin-server.${domainNamespace}.svc.cluster.local:7001/weblogic/ready\" | grep 200"
 
             Log.info(script, "domain readiness check success.")
 
