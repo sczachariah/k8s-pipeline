87485520b76e1ad1692c799437e1a467c7282ae3
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-07-03 14:36:35 +0530

    validation util impl

--- src/com/oracle/fmwk8s/common/Validation.groovy
+++ src/com/oracle/fmwk8s/common/Validation.groovy
@@ -2,5 +2,8 @@ package com.oracle.fmwk8s.common
 
 class Validation {
 
-    static def validateInputs(script){}
+    static def validateInputs(script){
+        Log.info(script, "begin validate inputs.")
+        Log.info(script, "validate inputs success.")
+    }
 }
