c52ca3ba839b0a2086aa082ba199c36043b5940b
srsanaga <srivalli.sanagavarapu@oracle.com>
2019-07-17 12:04:28 +0530

    To get the http and https port numbers

--- src/com/oracle/fmwk8s/env/IngressController.groovy
+++ src/com/oracle/fmwk8s/env/IngressController.groovy
@@ -98,12 +98,12 @@ class IngressController {
             Log.info(script, "begin get load balancer port.")
             script.sh "export KUBECONFIG=${script.env.KUBECONFIG}"
             script.sh "http=`kubectl describe service ${lbHelmRelease} --namespace ${domainNamespace}  | grep -i nodeport | grep 'http ' | awk -F/ '{print \$1}' | awk -F' ' '{print \$3}'`"
-            this.httplbPort = sh (
-                    script: '$http',
-                    returnStdout: true
-            ).trim()
+                       this.httplbPort = sh (
+                                            script: '$http',
+                                            returnStdout: true
+                                           ).trim()
             script.sh "https=`kubectl describe service ${lbHelmRelease} --namespace ${domainNamespace}  | grep -i nodeport | grep 'https' | awk -F/ '{print \$1}' | awk -F' ' '{print \$3}'`"
-            Log.info(script, ${this.httplbPort})
+            Log.info(script, this.httplbPort)
 
             Log.info(script, ${this.httpslbPort})
             Log.info(script, "get load balancer port success.")
