2a26b0539078c7da5e52535a977f7ed8ad168eb6
Swati Mukundan <swati.mukundan@oracle.com>
2019-07-31 12:01:15 +0530

    Updated OperatorIntegration.groovy with clustername and managed server base name for wls infra

--- src/com/oracle/fmwk8s/test/OperatorIntegration.groovy
+++ src/com/oracle/fmwk8s/test/OperatorIntegration.groovy
@@ -29,8 +29,10 @@ class OperatorIntegration {
                        sed -i \"s|\\\${OPERATOR_SA}|${Operator.operatorServiceAccount}|g\" operatorTest.properties && \
                        sed -i \"s|\\\${DOMAIN_NAME}|${Domain.domainName}|g\" operatorTest.properties && \
                        sed -i \"s|\\\${DOMAIN_NS}|${Domain.domainNamespace}|g\" operatorTest.properties && \
-                       sed -i \"s|\\\${CLUSTER_NAME}|${Common.productId}_cluster|g\" operatorTest.properties && \
-                       sed -i \"s|\\\${MANAGED_SERVER_NAME_BASE}|${Common.productId}_server|g\" operatorTest.properties && \
+                       #sed -i \"s|\\\${CLUSTER_NAME}|${Common.productId}_cluster|g\" operatorTest.properties && \
+                       #sed -i \"s|\\\${MANAGED_SERVER_NAME_BASE}|${Common.productId}_server|g\" operatorTest.properties && \
+                       sed -i \"s|\\\${CLUSTER_NAME}|cluster-1|g\" operatorTest.properties && \
+                       sed -i \"s|\\\${MANAGED_SERVER_NAME_BASE}|managed-server|g\" operatorTest.properties && \
                        sed -i \"s|\\\${ADMIN_SERVER_NAME}|admin-server|g\" operatorTest.properties && \
                        sed -i \"s|\\\${WEBLOGIC_CREDENTIALS_SECRET_NAME}|${Domain.weblogicCredentialsSecretName}|g\" operatorTest.properties && \
                        sed -i \"s|\\\${OPERATOR_HELM_RELEASE}|${Operator.operatorHelmRelease}|g\" operatorTest.properties && \
