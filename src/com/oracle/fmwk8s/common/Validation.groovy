1841eb762866b9877a18c04bc0c870df1d2f874f
srsanaga <srivalli.sanagavarapu@oracle.com>
2019-08-06 11:24:40 +0530

    Validation added

--- src/com/oracle/fmwk8s/common/Validation.groovy
+++ src/com/oracle/fmwk8s/common/Validation.groovy
@@ -7,7 +7,7 @@ class Validation {
             Log.info(script, "begin validate inputs.")
             if ("${operatorVersion}" == "2.1" && "${productName}" == "WLS-INFRA" ) {
                 Log.error(script, "Operator version 2.1 is not applicable for the product ${productName} .")
-                return false
+                System.exit(1)
             }
         }
         catch (exc) {
