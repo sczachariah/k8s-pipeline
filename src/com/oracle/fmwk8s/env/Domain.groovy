99787da5918415effce31b417c7b0070f03968c4
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-08-07 18:08:42 +0530

    use ssh creds for pulling samples

--- src/com/oracle/fmwk8s/env/Domain.groovy
+++ src/com/oracle/fmwk8s/env/Domain.groovy
@@ -14,6 +14,7 @@ class Domain {
 
     static pullSampleScripts(script) {
         script.git branch: "${Common.samplesBranch}",
+                credentialsId: 'sandeep.zachariah.ssh',
                 url: "${Common.samplesRepo}"
     }
 
