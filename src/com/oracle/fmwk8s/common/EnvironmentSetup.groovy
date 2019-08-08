a027415d14f21cfb5d5f07387a811771ddee3b8e
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-08-08 15:52:06 +0530

    changes to mount kubeconfig to a configmap

--- src/com/oracle/fmwk8s/common/EnvironmentSetup.groovy
+++ src/com/oracle/fmwk8s/common/EnvironmentSetup.groovy
@@ -3,6 +3,7 @@ package com.oracle.fmwk8s.common
 class EnvironmentSetup {
 
     static def isWaiting = false
+    static def kubeconfig
 
     static createNfsFolder(script, namespace, nfsHomeDir, nfsDomainDir) {
         try {
@@ -20,9 +21,40 @@ class EnvironmentSetup {
                         kubectl apply -f fmwk8s-mkdir-pod.yaml -n ${namespace}"
 
             Log.info(script, "create nfs folder success.")
+
+            mountKubeconfig(script, namespace)
         }
         catch (exc) {
             Log.error(script, "create nfs folder failed.")
+            throw exc
+        }
+        finally {
+        }
+    }
+
+    static mountKubeconfig(script, namespace) {
+        try {
+            Log.info(script, "begin mount kubeconfig.")
+
+            script.git branch: 'master',
+                    credentialsId: 'sandeep.zachariah.ssh',
+                    url: 'git@orahub.oraclecorp.com:fmw-platform-qa/fmw-k8s-pipeline.git'
+
+            this.kubeconfig = script.sh(
+                    script: "cat ${script.env.KUBECONFIG}",
+                    returnStdout: true
+            ).trim()
+
+            script.sh "export KUBECONFIG=${script.env.KUBECONFIG} && \
+                        cd kubernetes/framework && \
+                        sed -i \"s#%KUBECONFIG%#${this.kubeconfig}#g\" fmwk8s-kubeconfig-configmap.yaml && \
+                        cat fmwk8s-kubeconfig-configmap.yaml && \
+                        kubectl apply -f fmwk8s-kubeconfig-configmap.yaml -n ${namespace}"
+
+            Log.info(script, "mount kubeconfig success.")
+        }
+        catch (exc) {
+            Log.error(script, "mount kubeconfig failed.")
             throw exc
         }
         finally {
