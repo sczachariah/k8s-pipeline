33cb13f7feb92f9a6a6d3555d9677d6327f22c7f
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-09-09 16:00:31 +0530

    timeutility changes

--- src/com/oracle/fmwk8s/utility/TimerUtility.groovy
+++ src/com/oracle/fmwk8s/utility/TimerUtility.groovy
@@ -7,6 +7,7 @@ class TimerUtility {
     private static int maxTimerPeriod = 2
     private static Timer timer
     private static TimerTask timerTask
+    private static Object workflowScript
 
     static void main(String[] args) {
         TimerUtility timerUtility = new TimerUtility()
@@ -14,18 +15,20 @@ class TimerUtility {
     }
 
     void startTimer(script) {
+        this.workflowScript = script
         Log.info(script, "Timer check start")
         timer = new Timer()
         timerTask = new TimerTask() {
             @Override
             void run() {
-//                Log.info(script, "Inside the run method")
+                Log.info(workflowScript, "Inside the run method")
                 count++
                 Calendar cal = Calendar.getInstance()
                 cal.setTimeInMillis(System.currentTimeMillis())
                 String date = cal.get(Calendar.DATE) + "-" + cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.YEAR)
                 String time = cal.get(Calendar.HOUR) + "-" + cal.get(Calendar.MINUTE) + "-" + cal.get(Calendar.SECOND)
                 println("Date : " + date + " time : " + time + " count : " + count)
+                Log.info(workflowScript, "Date : " + date + " time : " + time + " count : " + count)
                 if (count > maxTimerPeriod) {
                     timer.cancel()
                     timer.purge()
