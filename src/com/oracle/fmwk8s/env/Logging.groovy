a2bafdf6eebfaeb97c8e4a931e617ace650a1bd6
srsanaga <srivalli.sanagavarapu@oracle.com>
2019-09-11 11:46:40 +0530

    for domain validation

--- src/com/oracle/fmwk8s/env/Logging.groovy
+++ src/com/oracle/fmwk8s/env/Logging.groovy
@@ -131,7 +131,7 @@ class Logging {
 
     static getDomainLogs(script, domainName, domainNamespace) {
         try {
-            Log.info(script, "begin get domain logs.")
+            Log.info(script, "begin get domain logs sfdfsfsfs.")
             script.sh "mkdir -p ${script.env.WORKSPACE}/${script.env.BUILD_NUMBER}/domain_logs && \
                        chmod 777 ${script.env.WORKSPACE}/${script.env.BUILD_NUMBER}/domain_logs && \
                        adminServer='adminServer' && \
