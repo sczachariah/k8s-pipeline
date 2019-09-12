b576fd5e6f182abc945057ea8caa1b98c5c5a58a
srsanaga <srivalli.sanagavarapu@oracle.com>
2019-09-12 13:06:11 +0530

    for domain validation

--- src/com/oracle/fmwk8s/env/Logging.groovy
+++ src/com/oracle/fmwk8s/env/Logging.groovy
@@ -135,7 +135,7 @@ class Logging {
             Log.info(script, "begin get domain logs sfdfsfsfs.")
             script.sh "mkdir -p ${script.env.WORKSPACE}/${script.env.BUILD_NUMBER}/domain_logs && \
                        chmod 777 ${script.env.WORKSPACE}/${script.env.BUILD_NUMBER}/domain_logs"
-            script.sh "adminServer=`echo \\`kubectl get pods -n ${domainNamespace} 2>&1 | grep \"${YamlUtility.domainInputsMap.get("adminServerName")}\"\\``\n \
+            script.sh "adminServer=`echo \\`kubectl get pods -n ${domainNamespace} 2>&1 | grep admin-server\\``\n \
                        echo \"\$adminServer\" "
             script.sh "ls ${script.env.WORKSPACE}/${script.env.BUILD_NUMBER}/domain_logs && \
                        cd ${script.env.WORKSPACE}/${script.env.BUILD_NUMBER}"
