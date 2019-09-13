43a30d39f87b9c826bce490792afb41d1c6a4d30
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-09-13 12:11:51 +0530

    add slack notifier

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,9 +1,9 @@
 def call(String buildResult) {
     if (buildResult == "FAILURE") {
         echo "INFO: Notifying Slack about FAILURE"
-        slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+        slackSend color: "danger", message: "Job: ${env.JOB_NAME} - build #${env.BUILD_NUMBER} - ${currentBuild.displayName} has failed.\nRefer Logs at ${env.BUILD_URL}."
     } else if (buildResult == "UNSTABLE") {
         echo "INFO: Notifying Slack about UNSTABLE"
-        slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+        slackSend color: "warning", message: "Job: ${env.JOB_NAME} - build #${env.BUILD_NUMBER} - ${currentBuild.displayName} is unstable.\nRefer Logs at ${env.BUILD_URL}."
     }
 }
