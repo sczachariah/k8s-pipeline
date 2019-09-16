e312004a395bb5139307dbf76afced4679a6ad36
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-09-16 13:23:33 +0530

    add utility for generating groovy doc

--- src/com/oracle/fmwk8s/utility/DocUtility.groovy
+++ src/com/oracle/fmwk8s/utility/DocUtility.groovy
@@ -13,7 +13,9 @@ class DocUtility {
                 url: 'git@orahub.oraclecorp.com:fmw-platform-qa/fmw-k8s-pipeline.git'
 
         script.sh "groovy doc.groovy"
+        script.sh "rm -rf /fmwk8s/groovydoc"
         script.sh "cp -r groovydoc /fmwk8s/"
+        script.sh "chmod -R 777 /fmwk8s/groovydoc"
 
         Log.info(script, "generate groovy doc success.")
     }
