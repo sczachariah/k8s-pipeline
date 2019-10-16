77bae807c8fc37404fa6fbae60fb64e912e68548
srsanaga <srivalli.sanagavarapu@oracle.com>
2019-10-16 11:19:54 +0530

    Global var changes

--- src/com/oracle/fmwk8s/common/Initializer.groovy
+++ src/com/oracle/fmwk8s/common/Initializer.groovy
@@ -3,6 +3,7 @@ package com.oracle.fmwk8s.common
 class Initializer {
 
     static def initialize(def script) {
+        Log.info(script, "Inside the Initializer")
         script.sh "echo Initializing Validation Framework"
         script.sh "touch /logs/jenkinsSample"
         Base.productName = this.env.PRODUCT_NAME
