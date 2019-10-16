1e7ff07be5b185dec919e47802213da6a2954f9d
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-10-16 14:38:42 +0530

    report utility to print domain urls

--- src/com/oracle/fmwk8s/utility/ReportUtility.groovy
+++ src/com/oracle/fmwk8s/utility/ReportUtility.groovy
@@ -12,20 +12,20 @@ class ReportUtility {
 """
         if (Common.productId.toString().equalsIgnoreCase("weblogic") ||
                 Common.productId.toString().equalsIgnoreCase("fmw-infrastructure")) {
-            domainURLs == domainURLs + """
+            domainURLs = domainURLs + """
 http://${Common.k8sMasterIP}:${IngressController.httplbPort}/console
 http://${Common.k8sMasterIP}:${IngressController.httplbPort}/weblogic/ready
 """
         } else if (Common.productId.toString().equalsIgnoreCase("soa")) {
             if (Domain.domainType.toString().equalsIgnoreCase("soa")) {
-                domainURLs == domainURLs + """
+                domainURLs = domainURLs + """
 http://${Common.k8sMasterIP}:${IngressController.httplbPort}/console
 http://${Common.k8sMasterIP}:${IngressController.httplbPort}/em
 http://${Common.k8sMasterIP}:${IngressController.httplbPort}/weblogic/ready
 """
             }
         }
-        domainURLs == domainURLs + """
+        domainURLs = domainURLs + """
 -----------------------------------
 """
         script.sh "printf \"%s\\n\" \"${domainURLs.toString()}\""
