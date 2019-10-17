576887d530f7e598494d0bed78019285126cc3bd
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-10-17 13:14:31 +0530

    update all urls for soa domain

--- src/com/oracle/fmwk8s/utility/ReportUtility.groovy
+++ src/com/oracle/fmwk8s/utility/ReportUtility.groovy
@@ -9,19 +9,28 @@ class ReportUtility {
     static printDomainUrls(script) {
         String domainURLs = """
 -----------Domain URL's------------
-"""
-        if (Common.productId.toString().equalsIgnoreCase("weblogic") ||
-                Common.productId.toString().equalsIgnoreCase("fmw-infrastructure")) {
-            domainURLs = domainURLs + """
-http://${Common.k8sMasterIP}:${IngressController.httplbPort}/console
 http://${Common.k8sMasterIP}:${IngressController.httplbPort}/weblogic/ready
+http://${Common.k8sMasterIP}:${IngressController.httplbPort}/console
 """
-        } else if (Common.productId.toString().equalsIgnoreCase("soa")) {
-            if (Domain.domainType.toString().equalsIgnoreCase("soa")) {
+        if (Common.productId.toString().equalsIgnoreCase("soa")) {
+            if (Domain.domainType.toString().toLowerCase().contains("soa")) {
                 domainURLs = domainURLs + """
-http://${Common.k8sMasterIP}:${IngressController.httplbPort}/console
 http://${Common.k8sMasterIP}:${IngressController.httplbPort}/em
-http://${Common.k8sMasterIP}:${IngressController.httplbPort}/weblogic/ready
+http://${Common.k8sMasterIP}:${IngressController.httplbPort}/soa-infra/
+http://${Common.k8sMasterIP}:${IngressController.httplbPort}/soa/composer
+http://${Common.k8sMasterIP}:${IngressController.httplbPort}/integration/worklistapp
+"""
+            }
+            if (Domain.domainType.toString().toLowerCase().contains("osb")) {
+                domainURLs = domainURLs + """
+http://${Common.k8sMasterIP}:${IngressController.httplbPort}/servicebus
+http://${Common.k8sMasterIP}:${IngressController.httplbPort}/sbconsole
+"""
+            }
+            if (Domain.domainType.toString().toLowerCase().contains("ess")) {
+                domainURLs = domainURLs + """
+http://${Common.k8sMasterIP}:${IngressController.httplbPort}/ess
+http://${Common.k8sMasterIP}:${IngressController.httplbPort}/EssHealthCheck
 """
             }
         }
