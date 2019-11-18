6bacc34d5524e62ea684118db76f825e4477febc
srivalli_sanagavarapu <srivalli.sanagavarapu@oracle.com>
2019-11-18 01:51:43 -0800

    call to email notification

--- src/com/oracle/fmwk8s/env/Domain.groovy
+++ src/com/oracle/fmwk8s/env/Domain.groovy
@@ -215,6 +215,7 @@ class Domain extends Common {
             // fix operator not managing the domain intermittently
             Operator.setDomainNamespace()
             isDomainReady()
+            ReportUtility.sendNotificationMailPostDomainCreation(script)
         }
         catch (exc) {
             Log.error("create/start " + productId + " domain failed.")
