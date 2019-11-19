b3866d8c63d5bed631dd7dbc4ddb54ad4ebac2e5
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-11-20 01:15:08 +0530

    remove wait in deploying nginx ingress controller

--- src/com/oracle/fmwk8s/env/IngressController.groovy
+++ src/com/oracle/fmwk8s/env/IngressController.groovy
@@ -139,7 +139,7 @@ class IngressController extends Common {
                     script: "helm init --client-only --skip-refresh --wait && \
                    helm repo update && \
                    helm install stable/nginx-ingress --name ${lbHelmRelease} --namespace ${domainNamespace} \
-                   --set controller.scope.namespace={${domainNamespace}} --debug --wait"
+                   --set controller.scope.namespace={${domainNamespace}} --debug"
             Log.info("deploy nginx ingress controller success.")
         }
         catch (exc) {
