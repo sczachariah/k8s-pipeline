bea59ded9dc9024fff7979e3320ef0ed52f9dbb1
priprabh <priyanka.p.prabhu@oracle.com>
2019-11-25 20:38:59 +0530

    refactored waitForTests logic as per JIRA story - FMWK8SVAL - 423

--- src/com/oracle/fmwk8s/test/Test.groovy
+++ src/com/oracle/fmwk8s/test/Test.groovy
@@ -101,13 +101,15 @@ class Test extends Common {
                 def fileExists = script.sh(
                         label: "check if the fmwk8s.completed file exists and is created after test execution",
                         script: "test -f ${Test.logDirectory}/fmwk8s.completed && echo ${fmwk8sCompletedFileExists} || echo ${fmwk8sCompletedFileNotExists}",
-                        returnStdout: true).trim().toString()
+                        returnStdout: true).trim()
                 countOfLooping ++
                 Log.info("value  of fileExists :: ${fileExists}")
                 Log.info("Iteration :: ${countOfLooping}")
                 if(fileExists == true) {
+                    Log.info("in if condition")
                     waitforfile  = false
                 }else if(fileExists == false){
+                    Log.info("in else if")
                     sleep 120
                     waitforfile  = true
                     continue
