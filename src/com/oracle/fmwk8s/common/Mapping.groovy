5f3ec8c49a236d1b67618bb4e8756e9ba968c256
priprabh <priyanka.p.prabhu@oracle.com>
2019-12-09 20:14:12 +0530

    revereted - refactored loadBalancer to mapping

--- src/com/oracle/fmwk8s/common/Mapping.groovy
+++ src/com/oracle/fmwk8s/common/Mapping.groovy
@@ -5,7 +5,6 @@ import com.oracle.fmwk8s.env.IngressController
 class Mapping {
     static Map<String, String> productIdMap = new HashMap<>()
     static Map<String, String> domainNameMap = new HashMap<>()
-    static Map<String, Object> loadBalancerMap = new HashMap<>()
     static Map<String, String> operatorVersionMap = new HashMap<>()
     static Map<String, String> operatorBranchMap = new HashMap<>()
 
@@ -21,11 +20,6 @@ class Mapping {
         domainNameMap.put("SOA", "soainfra")
         domainNameMap.put("WCP", "wcpinfra")
         domainNameMap.put("OIG", "oim")
-
-        loadBalancerMap.put("TRAEFIK", IngressController.deployTraefik())
-        loadBalancerMap.put("APACHE", IngressController.deployApache())
-        loadBalancerMap.put("VOYAGER", IngressController.deployVoyager())
-        loadBalancerMap.put("NGINX", IngressController.deployNginx())
 
         operatorVersionMap.put("2.3.0", "2.3.0")
         operatorVersionMap.put("2.4.0", "2.4.0")
