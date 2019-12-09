8b2cfb624710542ef6239101cd91cf92f2290312
priprabh <priyanka.p.prabhu@oracle.com>
2019-12-09 17:15:26 +0530

    added loadBalancer map in mapping class

--- src/com/oracle/fmwk8s/common/Mapping.groovy
+++ src/com/oracle/fmwk8s/common/Mapping.groovy
@@ -3,6 +3,7 @@ package com.oracle.fmwk8s.common
 class Mapping {
     static Map<String, String> productIdMap = new HashMap<>()
     static Map<String, String> domainNameMap = new HashMap<>()
+    static Map<String, String> loadBalancerMap = new HashMap<>()
 
     static {
         productIdMap.put("WLS", "weblogic")
@@ -16,5 +17,10 @@ class Mapping {
         domainNameMap.put("SOA", "soainfra")
         domainNameMap.put("WCP", "wcpinfra")
         domainNameMap.put("OIG", "oim")
+
+        loadBalancerMap.put("TRAEFIK", "Traefik")
+        loadBalancerMap.put("APACHE", "Apache")
+        loadBalancerMap.put("VOYAGER", "Voyager")
+        loadBalancerMap.put("NGINX", "Nginx")
     }
 }
