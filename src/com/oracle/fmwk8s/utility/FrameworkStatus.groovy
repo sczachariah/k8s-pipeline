5f36882d7a26f8662d10fdd02e2cf19a8b6fb80d
sandeep_zachariah <sandeep.zachariah@oracle.com>
2019-12-11 16:23:25 +0530

    changes to framework status json

--- src/com/oracle/fmwk8s/utility/FrameworkStatus.groovy
+++ src/com/oracle/fmwk8s/utility/FrameworkStatus.groovy
@@ -6,26 +6,26 @@ import groovy.json.JsonOutput
 class FrameworkStatus {
     String runId
     String cloud
-    String operatorVersion
-    String productName
-    String domainType
-    String productImage
+    Map<String, String> parameters
 
     FrameworkStatus() {
         this.runId = Base.runId
         this.cloud = Base.cloud
-        this.operatorVersion = Base.operatorVersion
-        this.productName = Base.productName
-        this.domainType = Base.domainType
-        this.productImage = Base.productImage
+
+        parameters = new HashMap<>()
+        parameters.put("operatorVersion", Base.operatorVersion)
+        parameters.put("productName", Base.productName)
+        parameters.put("domainType", Base.domainType)
+        parameters.put("productImage", Base.productImage)
     }
 
     static getFrameworkStatusJson(script) {
         FrameworkStatus frameworkStatus = new FrameworkStatus();
         def json = JsonOutput.toJson(frameworkStatus)
+        def prettyJson = JsonOutput.prettyPrint(json)
 
         script.sh label: "print framework status json",
-                script: "echo \"${json}\""
+                script: "echo \"${prettyJson}\""
     }
 }
 
