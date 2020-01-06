1cbf3a946e5236d1d5fda6211d4b81a1b3c5aa40
sandeep_zachariah <sandeep.zachariah@oracle.com>
2020-01-06 10:40:54 +0530

    changes to expose t3 channel

--- src/com/oracle/fmwk8s/utility/YamlUtility.groovy
+++ src/com/oracle/fmwk8s/utility/YamlUtility.groovy
@@ -58,7 +58,7 @@ class YamlUtility implements Serializable {
         map.put("logHome", "/shared/logs/" + domainName.toString())
         map.put("t3ChannelPort", 31012)
 //        https://bug.oraclecorp.com/pls/bug/webbug_edit.edit_info_top?rptno=30176526
-        map.put("exposeAdminT3Channel", false)
+        map.put("exposeAdminT3Channel", true)
         map.put("t3PublicAddress", "fmwk8s.us.oracle.com")
         map.put("namespace", domainNamespace.toString())
         map.put("persistentVolumeClaimName", domainName.toString() + "-" + domainNamespace.toString() + "-pvc")
