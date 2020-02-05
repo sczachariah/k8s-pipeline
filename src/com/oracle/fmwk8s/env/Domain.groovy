2ba8b1bf7760cfce349aa2a82c2ddbb019d00e19
priprabh <priyanka.p.prabhu@oracle.com>
2020-02-05 12:46:51 +0530

    added sleep...

--- src/com/oracle/fmwk8s/env/Domain.groovy
+++ src/com/oracle/fmwk8s/env/Domain.groovy
@@ -178,7 +178,7 @@ class Domain extends Common {
 
             // create
             Log.info("begin create " + productId + " domain.")
-            sleep 400
+            sleep 1000
             script.sh label: "create domain",
                     script: "./kubernetes/samples/scripts/create-${productId}-domain/${samplesDirectory}/create-domain.sh -i create-domain-inputs.yaml -o script-output-directory"
             script.sh label: "prepare domain yaml file",
